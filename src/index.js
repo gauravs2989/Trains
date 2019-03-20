2a004ab5b4842c2326a33f9de9ec4265aaebd558
gauravs2989 <gaurav.savadi@gmail.com>
2019-03-20 17:41:30 -0400

    Get data front end

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const Map = require("./server/Map");
+
 app.use(express.static(path.join(__dirname, '../dist/train-game/')));
 
 // Catch all routes and return it to the index page
---
+++ src/server/Map.js
@@ -0,0 +1,6 @@
+function Map() {
+    console.log("Creating a map");
+    this._foo = "Foo";
+}
+
+module.exports = Map;
