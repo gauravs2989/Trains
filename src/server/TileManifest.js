97bae52ed32e242d0ae4731d39090c63556b7abf
Gaurav Savadi <gaurav.savadi@gmail.com>
2019-03-21 23:19:38 -0400

    Start creating tile manifest

--- src/server/TileManifest.js
+++ src/server/TileManifest.js
@@ -1,17 +1,25 @@
 const CityTile = require("./CityTile");
 const Sides = require("./SideEnum");
 
+var manifest = {
+    "erie": new CityTile("yellow", 10, [Sides.NorthEast, Sides.SouthWest, Sides.West], 2),
+    "buffalo": new CityTile("red", 30, [Sides.SouthWest, Sides.West], 0),
+    5: new CityTile("yellow", 20, [Sides.NorthEast, Sides.NorthWest], 1),
+    6: new CityTile("yellow", [Sides.NorthEast, Sides.West], 1)
+}
+
 /**
  * The tile manifest maintains a list of all the tiles in the game and provides information about
  * which upgrades are eligible for a given tile
  */
 class TileManifest {
+
     static getDefaultErieTile() {
-        return new CityTile("yellow", 10, [Sides.NorthEast, Sides.SouthWest, Sides.West], 2);
+        return manifest["erie"];
     }
 
     static getDefaultBuffaloTile() {
-        return new CityTile("red", 30, [Sides.SouthWest, Sides.West], 0);
+        return manifest["buffalo"];
     }
 }
 
