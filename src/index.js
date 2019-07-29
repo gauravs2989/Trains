607d962a94e7bd0f9ff8a70ab349112c9158aea2
gauravs2989 <gaurav.savadi@gmail.com>
2019-07-29 13:13:00 -0400

    Refresh

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const Map = require("@server/game/Map");
-const HexModel = require("@server/model/HexModel");
-const Network = require("@server/model/Network");
-const NetworkManager = require("@server/model/NetworkManager"); 
-
 app.use(express.static(path.join(__dirname, '../dist/train-game/')));
 
 // Catch all routes and return it to the index page
@@ -18,11 +13,6 @@ app.get('/', (req, res)=> {
 
 app.get('/server', (req, res)=> {
     console.log("Requesting data");
-    var network = new Network();
-    var hexModel = new HexModel();
-    var networkManager = new NetworkManager(network, hexModel);
-    var map = new Map(hexModel);
-    res.send(map.getHexes());
 });
 
 const port = process.env.PORT || 4600;
--- src/server/events/EventManager.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require("events");
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on('uncaughtException', (err)=> {
-    console.log(err);
-});
-
-module.exports = eventEmitter;
